62bf17c87014afa1b16775402e7a07636d021648
chichkanov <agchichkanov@gmail.com>
2017-03-15 22:35:46 +0300

    fixed sign, added Picasso

--- app/src/main/java/com/chichkanov/tinkoff_fintech/MainActivity.java
+++ app/src/main/java/com/chichkanov/tinkoff_fintech/MainActivity.java
@@ -1,12 +1,19 @@
 package com.chichkanov.tinkoff_fintech;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
     }
 }
