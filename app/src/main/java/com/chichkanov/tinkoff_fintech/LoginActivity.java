b0029b380eb93143bd23600a8b835a7414a4a9e9
chichkanov <agchichkanov@gmail.com>
2017-03-20 17:50:47 +0300

    created login screen

---
+++ app/src/main/java/com/chichkanov/tinkoff_fintech/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.chichkanov.tinkoff_fintech;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by chichkanov on 20.03.17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText login;
+    private EditText password;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = (EditText) findViewById(R.id.edit_text_login);
+        password = (EditText) findViewById(R.id.edit_text_password);
+        button = (Button) findViewById(R.id.btn_enter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNextScreen();
+            }
+        });
+    }
+
+    private void startNextScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("LOGIN", login.getText().toString());
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/chichkanov/tinkoff_fintech/MainActivity.java
+++ app/src/main/java/com/chichkanov/tinkoff_fintech/MainActivity.java
@@ -2,17 +2,24 @@ package com.chichkanov.tinkoff_fintech;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+    private void initRecyclerView(){
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_dialogs);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
